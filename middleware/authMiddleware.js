d2d8d9baf16c4bc9c0e35e3793512d0c836b43ee
soyesh-1 <soyeshxrestha@gmail.com>
2025-06-13 22:43:48 +0545

    adding middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+// backend/middleware/authMiddleware.js
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+module.exports = function (req, res, next) {
+  // Get token from header
+  const token = req.header('x-auth-token');
+
+  // Check if no token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
+
+  // Verify token
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.user; // Attach user payload to request object
+    next(); // Move on to the next piece of middleware or the controller
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
+};
